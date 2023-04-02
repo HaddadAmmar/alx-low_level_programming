de2d941b72d396e132f9747b95da1b792905d695
Ammar <me@testing.com>
2023-04-02 09:20:48 -0700

    hhh

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,15 @@
 */
 void print_rev(char *s)
 {
-        int len = 0;
+int len = 0;
 
-        while (s[len] != '\0')
-                len++;
-
-        for (int i = len - 1; i >= 0; i--)
-                putchar(s[i]);
-
-        putchar('\n');
+while (s[len] != '\0')
+{
+len++;
+}
+for (int i = len - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
 }
