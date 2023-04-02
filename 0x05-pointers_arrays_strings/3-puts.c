53990a1901bd5fe700b4b432edd363a20c557285
Ammar <me@testing.com>
2023-04-02 09:12:49 -0700

    ghgh

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,13 +5,10 @@
 */
 void print_rev(char *s)
 {
-        int len = 0;
-
-        while (s[len] != '\0')
-                len++;
-
-        for (int i = len - 1; i >= 0; i--)
-                putchar(s[i]);
-
-        putchar('\n');
+int len = 0;
+while (s[len] != '\0')
+len++;
+for (int i = len - 1; i >= 0; i--)
+putchar(s[i]);
+putchar('\n');
 }
